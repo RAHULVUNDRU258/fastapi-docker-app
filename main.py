73107f715ee9af4942259c8d7c173a8e50d17655
vundru vamsi <vundrurahul2511@gmail.com>
2026-04-14 23:00:29 +0530

    fastapi docker project - first working version

---
+++ main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"message": "It works!"}
+
+@app.post ("/add-task")
+def add_task(task: str):
+    tasks.append(task)
+    return {"tasks": tasks}
+
+@app.get("/tasks")
+def get_tasks():
+    return {"tasks": tasks}
